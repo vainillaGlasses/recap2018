eb7d15d6757a53f96e21f35baf417c19ba009f2c
Samantha Arburola <samantha@estudiomanati.com>
2019-03-15 13:50:18 -0600

    mobile breakpoint 480

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const mobile = "/css/mobile.css";
 const desktop = "css/style.css";
-if(screen.width <= 680){
+if(screen.width <= 480){
     document.getElementById("stylesheet").setAttribute("href", mobile);
     document.getElementById("drupal-title").innerHTML = "Drupal Camp <br> Costa Rica 2018";
     document.getElementById("cancion-text").innerHTML = "Bohemian Rhapsody</span> <br>(Aunque esa es la canción de Manatí desde que encontramos el ahora extinto Ko-zin)";
